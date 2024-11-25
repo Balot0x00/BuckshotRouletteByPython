60402948d96ee692b2b278df10fbc709258673e8
nzbstn <7802006+fangbaymin@user.noreply.gitee.com>
2024-11-25 16:45:22 +0800

    修复: 道具选取范围超过持有道具的范围

--- utils/util.py
+++ utils/util.py
@@ -7,11 +7,13 @@ def UserInput(prompt, lt=[]):
     :param prompt: 提示信息
     :return: 输入的内容
     """
+    log.debug(f"lt: {lt}")
     while True:
         num = input(prompt)
+        
         if not num.isdigit():
             log.warning("非法字符，请重新输入")
-        elif not len(lt) == 0 and not int(num) in lt:
+        elif not len(lt) == 0 and not num in lt:
             log.warning("超出范围, 请重新输入")
         else:
             return num
